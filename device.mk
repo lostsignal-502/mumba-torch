58f078188f0677cf0279ff3cfce00b8e46f26745
yukamnua <razenxizu@gmail.com>
2026-09-07 12:07:11 +0000

    a

--- device.mk
+++ device.mk
@@ -11,6 +11,9 @@ $(call inherit-product, hardware/qcom-caf/common/common.mk)
 # Dolby Atmos
 $(call inherit-product, hardware/dolby/dolby.mk)
 
+# GameBar Performance Overlay
+$(call inherit-product, packages/apps/GameBar/gamebar.mk)
+
 # Reduce system server verbosity
 PRODUCT_SYSTEM_SERVER_DEBUG_INFO := false
 
@@ -90,11 +93,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_module_config_primary.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/audio_module_config_primary.xml \
     $(LOCAL_PATH)/audio/audio_effects_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_parrot/audio_effects_config.xml \
     $(LOCAL_PATH)/audio/audio_effects_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_parrotlite/audio_effects_config.xml
-
-# Axion Kernel Manager
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/kernel/ax_kernel_manager_parrot66.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/ax_kernel_manager.xml \
-    $(LOCAL_PATH)/rootdir/etc/ax_init_parrot66.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/ax_init_parrot66.rc
 
 PRODUCT_COPY_FILES += \
     $(CONFIG_HAL_SRC_DIR)/card-defs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/card-defs.xml \
